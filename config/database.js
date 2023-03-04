a0500173067de591e26e113de13cc6deebb0c451
anderCM <andercm15@gmail.com>
2023-03-04 16:53:37 -0500

    Minor changes

--- config/database.js
+++ config/database.js
@@ -11,16 +11,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-/* module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3307),
-      database: env('DATABASE_NAME', 'escorpion_ecommerce'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      //ssl: env.bool('DATABASE_SSL', true),
-    },
-  },
-}); */
